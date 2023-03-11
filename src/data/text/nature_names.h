034e871063c1f7644af301e938264976ecfda41c
Acimut <79547805+Acimut@users.noreply.github.com>
2023-03-10 19:55:33 -0500

    Update nature_names.h

--- src/data/text/nature_names.h
+++ src/data/text/nature_names.h
@@ -1,3 +1,4 @@
+#if ENGLISH
 static const u8 sHardyNatureName[] = _("HARDY");
 static const u8 sLonelyNatureName[] = _("LONELY");
 static const u8 sBraveNatureName[] = _("BRAVE");
@@ -23,6 +24,33 @@ static const u8 sGentleNatureName[] = _("GENTLE");
 static const u8 sSassyNatureName[] = _("SASSY");
 static const u8 sCarefulNatureName[] = _("CAREFUL");
 static const u8 sQuirkyNatureName[] = _("QUIRKY");
+#elif SPANISH
+static const u8 sHardyNatureName[] = _("FUERTE");
+static const u8 sLonelyNatureName[] = _("HURAÑA");
+static const u8 sBraveNatureName[] = _("AUDAZ");
+static const u8 sAdamantNatureName[] = _("FIRME");
+static const u8 sNaughtyNatureName[] = _("PÍCARA");
+static const u8 sBoldNatureName[] = _("OSADA");
+static const u8 sDocileNatureName[] = _("DÓCIL");
+static const u8 sRelaxedNatureName[] = _("PLÁCIDA");
+static const u8 sImpishNatureName[] = _("AGITADA");
+static const u8 sLaxNatureName[] = _("FLOJA");
+static const u8 sTimidNatureName[] = _("MIEDOSA");
+static const u8 sHastyNatureName[] = _("ACTIVA");
+static const u8 sSeriousNatureName[] = _("SERIA");
+static const u8 sJollyNatureName[] = _("ALEGRE");
+static const u8 sNaiveNatureName[] = _("INGENUA");
+static const u8 sModestNatureName[] = _("MODESTA");
+static const u8 sMildNatureName[] = _("AFABLE");
+static const u8 sQuietNatureName[] = _("MANSA");
+static const u8 sBashfulNatureName[] = _("TÍMIDA");
+static const u8 sRashNatureName[] = _("ALOCADA");
+static const u8 sCalmNatureName[] = _("SERENA");
+static const u8 sGentleNatureName[] = _("AMABLE");
+static const u8 sSassyNatureName[] = _("GROSERA");
+static const u8 sCarefulNatureName[] = _("CAUTA");
+static const u8 sQuirkyNatureName[] = _("RARA");
+#endif
 
 const u8 *const gNatureNamePointers[] = 
 {
